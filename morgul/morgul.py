155445a066939f01872433aa97080c97f78e6878
Nicholas Devenish <ndevenish@gmail.com>
2023-06-16 14:32:44 +0100

    CLI: morgul only, and custom order for commands

--- morgul/morgul.py
+++ morgul/morgul.py
@@ -2,7 +2,18 @@ import typer
 
 from . import morgul_correct, morgul_gainmap, morgul_prepare
 
-app = typer.Typer(add_completion=False)
+
+class NaturalOrderGroup(typer.core.TyperGroup):
+    """Custom grouping class for typer for ordered commands"""
+
+    def list_commands(self, _ctx):
+        return self.commands.keys()
+
+
+app = typer.Typer(
+    cls=NaturalOrderGroup,
+    no_args_is_help=True,
+)
 
 app.command()(morgul_gainmap.gainmap)
 app.command()(morgul_prepare.prepare)
