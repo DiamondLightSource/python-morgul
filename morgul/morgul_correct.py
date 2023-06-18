44e8b190fcee52d85637650a39ae6e47ffffc96a
Nicholas Devenish <ndevenish@gmail.com>
2023-06-18 22:57:11 +0100

    Start tidying mask, Pedestal convenience class

--- morgul/morgul_correct.py
+++ morgul/morgul_correct.py
@@ -1,16 +1,117 @@
 import logging
 from pathlib import Path
-from typing import Annotated
+from typing import Annotated, Any, cast, overload
 
 import h5py
 import hdf5plugin
 import numpy
+import numpy.typing
 import tqdm
 import typer
 
-from .config import psi_gain_maps
+from .config import Detector, get_known_modules_for_detector, psi_gain_maps
 
 logger = logging.getLogger(__name__)
+
+
+class PedestalCorrections:
+    """
+    Read in pedestal correction files, and make it easy to retrieve the data
+    """
+
+    detector: Detector
+    filename: Path
+    _tables: dict[tuple[float, str, int], numpy.typing.NDArray]
+
+    def __init__(self, detector: Detector, filename: Path):
+        self.detector = detector
+        self.filename = filename
+
+        valid_modules = get_known_modules_for_detector(detector)
+        self._tables = {}
+        with h5py.File(filename, "r") as f:
+            # For now, just read all data into memory
+            exposure_time = f["exptime"][()]
+            for mod in [x for x in valid_modules if x in f]:
+                # Read any gain-mode pedestals out of this file
+                for gainmode in range(3):
+                    if f"pedestal_{gainmode}" in f[mod]:
+                        self._tables[exposure_time, mod, gainmode] = numpy.copy(
+                            f[mod][f"pedestal_{gainmode}"]
+                        )
+
+    @property
+    def exposure_times(self):
+        return set(x for x, _, _ in self._tables)
+
+    def get_pedestal(
+        self, exposure_time: float, module: str, gain_mode: int
+    ) -> numpy.typing.NDArray:
+        try:
+            return self._tables[exposure_time, module, gain_mode]
+        except KeyError:
+            raise KeyError(
+                f"No pedestal data for (exposure: {exposure_time}, module: {module}, mode: {gain_mode})"
+            )
+
+    def has_pedestal(
+        self, exposure_time: float, module: str, gain_mode: int | None = None
+    ) -> bool:
+        """Do we contain the pedestal correction tables for a specific module?"""
+        if gain_mode is not None:
+            return (exposure_time, module, gain_mode) in self._tables
+        else:
+            # Only say yes if we have all pedestal data (which should always be
+            # true - we should never generate a file without all three)
+            matches = [
+                g for t, m, g in self._tables if t == exposure_time and m == module
+            ]
+            assert len(matches) == 3, f"Expected 3 pedestal maps, got {len(matches)}"
+            return True
+
+    def get_pedestals_dict(
+        self, exposure_time: float, module: str
+    ) -> dict[str, numpy.typing.NDArray]:
+        pass
+
+    @overload
+    def __getitem__(
+        self, key: float | tuple[float]
+    ) -> dict[str, dict[int, numpy.typing.NDArray]]:
+        pass
+
+    @overload
+    def __getitem__(self, key: tuple[float, str]) -> dict[int, numpy.typing.NDArray]:
+        pass
+
+    @overload
+    def __getitem__(self, key: tuple[float, str, int]) -> numpy.typing.NDArray:
+        pass
+
+    def __getitem__(
+        self, key: float | tuple[float] | tuple[float, str] | tuple[float, str, int]
+    ):
+        output: Any = {}
+        if isinstance(key, float):
+            key = (key,)
+        if len(key) == 1:
+            # Get all entries for one exposure time
+            for module, gainmode in [(m, g) for e, m, g in self._tables if e == key[0]]:
+                output.setdefault(module, dict())[gainmode] = self._tables[
+                    key[0], module, gainmode
+                ]
+        elif len(key) == 2:
+            # Get all entries for one exposure time
+            key = cast(tuple[float, str], key)
+            for gainmode in [
+                g for e, m, g in self._tables if e == key[0] and m == key[1]
+            ]:
+                # output.setdefault(module, dict())[gainmode] = self._tables(key, module, gainmode)
+                output[gainmode] = self._tables[key[0], key[1], gainmode]
+        elif len(key) == 3:
+            # assert isinstance(key, tuple)
+            return self._tables[cast(tuple[float, str, int], key)]
+        return output
 
 
 def get_pedestals(pedestal_file):
@@ -45,23 +146,28 @@ def embiggen(packed):
     return bigger
 
 
-def correct_frame(raw, pedestals, g012, energy):
+def correct_frame(
+    raw: numpy.typing.NDArray[numpy.uint16],
+    pedestals: dict[int, numpy.typing.NDArray],
+    g012,
+    energy: float,
+):
     """Correct pixel values to photons in frame"""
 
     gain = numpy.right_shift(raw, 14)
-    m0 = pedestals["p0"] != 0
-    frame = ((gain == 0) * (numpy.bitwise_and(raw, 0x3FFF)) * m0 - pedestals["p0"]) / (
+    m0 = pedestals[0] != 0
+    frame = ((gain == 0) * (numpy.bitwise_and(raw, 0x3FFF)) * m0 - pedestals[0]) / (
         g012[0] * energy
     )
-    if "p1" in pedestals:
-        m1 = pedestals["p1"] != 0
+    if 1 in pedestals:
+        m1 = pedestals[1] != 0
         frame += (
-            (gain == 1) * ((numpy.bitwise_and(raw, 0x3FFF)) * m1 - pedestals["p1"])
+            (gain == 1) * ((numpy.bitwise_and(raw, 0x3FFF)) * m1 - pedestals[1])
         ) / (g012[1] * energy)
-    if "p2" in pedestals:
-        m2 = pedestals["p2"] != 0
+    if 2 in pedestals:
+        m2 = pedestals[2] != 0
         frame += (
-            (gain == 3) * ((numpy.bitwise_and(raw, 0x3FFF)) * m2 - pedestals["p2"])
+            (gain == 3) * ((numpy.bitwise_and(raw, 0x3FFF)) * m2 - pedestals[2])
         ) / (g012[2] * energy)
     return frame
 
--- morgul/morgul_mask.py
+++ morgul/morgul_mask.py
@@ -1,43 +1,56 @@
+import contextlib
+import logging
 from pathlib import Path
 from typing import Annotated, Optional
 
 import h5py
 import numpy
+import numpy.typing
 import tqdm
 import typer
 
-from .config import get_config, get_detector, psi_gain_maps
-from .morgul_correct import correct_frame
+from .config import (
+    get_detector,
+    get_module_info,
+    psi_gain_maps,
+)
+from .morgul_correct import PedestalCorrections, correct_frame
+from .util import NC, R
+
+logger = logging.getLogger(__name__)
 
 
-def _calculate(filename, pedestals, gain_maps, energy):
+def _calculate(
+    h5: h5py.Group,
+    pedestals: dict[int, numpy.typing.NDArray],
+    gain_maps: dict[str, numpy.typing.NDArray],
+    energy: float,
+):
     """Use the data given in filename to derive a trusted pixel mask"""
 
-    with h5py.File(filename) as f:
-        s = f["data"].shape
+    data = h5["data"]
+    s = data.shape
 
-        # fetch the correct gain maps for this module
-        image = numpy.zeros(shape=(s[1], s[2]), dtype=numpy.float64)
-        square = numpy.zeros(shape=(s[1], s[2]), dtype=numpy.float64)
+    # fetch the correct gain maps for this module
+    image = numpy.zeros(shape=(s[1], s[2]), dtype=numpy.float64)
+    square = numpy.zeros(shape=(s[1], s[2]), dtype=numpy.float64)
 
-        gain_mode = f["gainmode"][()].decode()
-        assert (
-            gain_mode == "dynamic"
-        ), f"Data with gain mode 'dynamic' (this is {gain_mode}) required for mask calculation"
+    gain_mode = h5["gainmode"][()].decode()
+    assert (
+        gain_mode == "dynamic"
+    ), f"Data with gain mode 'dynamic' (this is {gain_mode}) required for mask calculation"
 
-        d = f["data"]
-
-        # compute sum, sum of squares down stack
-        for j in tqdm.tqdm(range(d.shape[0]), desc="Mask"):
-            frame = correct_frame(d[j], pedestals, gain_maps, energy)
-            image += frame
-            square += numpy.square(frame)
-        mean = image / d.shape[0]
-        var = square / d.shape[0] - numpy.square(mean)
-        mean[mean == 0] = 1
-        disp = var / mean
-        print(f"Masking {numpy.count_nonzero(disp > 3)} pixels")
-        return (disp > 3).astype(numpy.uint32)
+    # compute sum, sum of squares down stack
+    for j in tqdm.tqdm(range(data.shape[0]), desc="Mask"):
+        frame = correct_frame(data[j], pedestals, gain_maps, energy)
+        image += frame
+        square += numpy.square(frame)
+    mean = image / data.shape[0]
+    var = square / data.shape[0] - numpy.square(mean)
+    mean[mean == 0] = 1
+    disp = var / mean
+    print(f"Masking {numpy.count_nonzero(disp > 3)} pixels")
+    return (disp > 3).astype(numpy.uint32)
 
 
 def mask(
@@ -60,25 +73,67 @@ def mask(
         Optional[Path],
         typer.Option(
             "-o",
-            help="Name for the output HDF5 file. Default: <detector>_<module>_<exptime>ms_mask.h5",
+            help="Name for the output HDF5 file. Default: <detector>_<exptime>ms_mask.h5",
         ),
     ] = None,
 ):
     """Prepare a pixel mask from flatfield data"""
     print(f"Running generation for: {flat}")
-    # assert "p0" in pedestals
+
     detector = get_detector()
     gain_maps = psi_gain_maps(detector)
 
-    with h5py.File(flat, "r") as _f:
-        r = int(_f["row"][()])
-        c = int(_f["column"][()])
+    # flat_module_map: dict[str, h5py.Group] = {}
 
-    config = get_config()
-    module = config[f"{detector}-{c}{r}"]["module"]
-    maps = gain_maps[module]
-    m = _calculate(flat, maps)
+    exposure_time: float | None = None
 
-    output = Path(f"{detector}_{module}_mask.h5")
-    with h5py.File(output, "w") as f:
-        f.create_dataset("mask", data=m)
+    pedestals = PedestalCorrections(detector, pedestal)
+
+    # Open the flat-field, and validate that they are matches
+    with contextlib.ExitStack() as stack:
+        # Read the data from the pedestal file
+        # with h5py.File(pedestal, "r") as f:
+        #     # Fix the exposure time for this pedestal file
+        #     exposure_time = f["exptime"][()]
+
+        for filename in flat:
+            h5 = stack.enter_context(h5py.File(filename, "r"))
+            exptime = h5["exptime"][()]
+            # Validate that this exposure time is identical and present in the pedestal data
+            if exposure_time is None:
+                exposure_time = exptime
+                if exptime not in pedestals.exposure_times:
+                    logger.error(
+                        f"{R}Error: Flatfield has exposure {exptime} but pedestal data only contains ({', '.join(pedestals.exposure_times)}){NC}"
+                    )
+                    raise typer.Abort()
+            elif exptime != exposure_time:
+                logger.error(
+                    f"{R}Error: flatfield file {filename} exposure time ({exptime} does not match the pedestal data ({exposure_time})"
+                )
+                raise typer.Abort()
+            # Validate that this module is present in the pedestal data
+            module: str = get_module_info(
+                detector, int(h5["column"][()]), int(h5["row"][()])
+            )["module"]
+            if not pedestals.has_pedestal(exposure_time, module):
+                logger.error(
+                    f"{R}Error: No data in pedestal file for module {module} for exposure {exposure_time*1000:g} ms{NC}"
+                )
+
+            if h5["gainmode"][()].decode() != "dynamic":
+                logger.error(
+                    f"{R}Error: Data in file {filename} is not gainmode=dynamic (instead is '{h5['gainmode'][()]}') and is not suitable for masking{NC}"
+                )
+                raise typer.Abort()
+            # We know that we have data for this flatfield. Do the masking calculation.
+            _calculate(h5, pedestals[exposure_time, module], gain_maps[module], energy)
+
+    # config = get_config()
+    # module = config[f"{detector}-{c}{r}"]["module"]
+    # maps = gain_maps[module]
+    # m = _calculate(flat, maps)
+
+    # output = Path(f"{detector}_{module}_mask.h5")
+    # with h5py.File(output, "w") as f:
+    #     f.create_dataset("mask", data=m)
