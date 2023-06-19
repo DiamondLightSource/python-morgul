9923144726e37fa23705e6c0e4f7701c4721901f
Nicholas Devenish <ndevenish@gmail.com>
2023-06-19 16:30:58 +0100

    Teach view about file kinds

--- morgul/view.py
+++ morgul/view.py
@@ -1,3 +1,5 @@
+import enum
+import logging
 from pathlib import Path
 from typing import Annotated
 
@@ -5,14 +7,76 @@ import h5py
 import napari
 import typer
 
+from . import config
+from .util import NC, B, G, R
+
+logger = logging.getLogger(__name__)
+
+
+class FileKind(enum.Enum):
+    # UNKNOWN = enum.auto()
+    MASK = enum.auto()
+    PEDESTAL = enum.auto()
+    RAW = enum.auto()
+    CORRECTED = enum.auto()
+
+
+def determine_kinds(root: h5py.Group) -> set[FileKind]:
+    """Given an HSD5 data group, work out what kind of data it contains"""
+    detector = config.get_detector()
+    modules = config.get_known_modules_for_detector(detector)
+    kinds = set()
+    # Work out what sort of file we have
+    module_subgroups = [
+        x for x in modules if isinstance(root[x], h5py.Group) and x in root
+    ]
+    for module in module_subgroups:
+        if "mask" in root[module]:
+            kinds.add(FileKind.MASK)
+        if any(
+            x.startswith("pedestal_")
+            for x in root[module]
+            if isinstance(root[module][x], h5py.Dataset)
+        ):
+            kinds.add(FileKind.PEDESTAL)
+    if "data" in root and isinstance(root["data"], h5py.Dataset):
+        if root["data"].attrs.get("corrected", False):
+            kinds.add(FileKind.CORRECTED)
+        else:
+            kinds.add(FileKind.RAW)
+
+    return kinds
+
+
+def determine_kind(root: h5py.Group) -> FileKind | None:
+    """Return a single file kind"""
+    kinds = determine_kinds(root)
+    if kinds:
+        return sorted(kinds, key=lambda x: x.value)[-1]
+    return None
+
 
 def view(filename: Annotated[Path, typer.Argument(help="Data file to view")]):
     """Launch a napari-based viewer"""
+
     with h5py.File(filename, "r") as f:
         viewer = napari.Viewer()
-        for module in "M420", "M418":
-            for mode in 0, 1, 2:
-                viewer.add_image(
-                    f[module][f"pedestal_{mode}"][()], name=f"{module}/{mode}"
-                )
+        kind = determine_kind(f)
+        if kind is None:
+            logger.error(f"{R}Error: Could not determine file kind for {filename}{NC}")
+            raise typer.Abort()
+
+        logger.info(f"Opening {B}{filename}{NC} as {G}{kind.name.title()}{NC}")
+        if kind == FileKind.PEDESTAL:
+            for module in "M420", "M418":
+                for mode in 0, 1, 2:
+                    viewer.add_image(
+                        f[module][f"pedestal_{mode}"][()], name=f"{module}/{mode}"
+                    )
+        else:
+            logger.error(
+                f"{R}Error: File kind {kind.name} is not currently supported{NC}"
+            )
+            raise typer.Abort()
+
         napari.run()
