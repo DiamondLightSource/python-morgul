3afc36834a17d134f91715a0699a47c1c22832e8
Nicholas Devenish <ndevenish@gmail.com>
2023-06-20 11:43:56 +0100

    Update dumper script

--- dump_files.py
+++ dump_files.py
@@ -1,12 +1,20 @@
 import datetime
 import itertools
+import os
 from pathlib import Path
+from typing import Annotated, Optional
 
 import h5py
 import typer
 
 
-def main(target_folders: list[Path]):
+def main(
+    target_folders: list[Path], root: Annotated[Optional[Path], typer.Option()] = None
+):
+    if root is None and "VISIT_DATA_ROOT" in os.environ:
+        root = Path(os.environ["VISIT_DATA_ROOT"])
+        print(f"Using as data root: {root}")
+
     entries: dict[datetime.datetime, dict] = {}
     for filename in itertools.chain(*[x.glob("**/*.h5") for x in target_folders]):
         with h5py.File(filename, "r") as f:
@@ -16,6 +24,13 @@ def main(target_folders: list[Path]):
                 if filename.name > entries[timestamp]["path"].name:
                     continue
             exptime = f["exptime"][()]
+
+            if root:
+                filename = filename.relative_to(root)
+            else:
+                root = Path(*filename.parts[:6])
+                filename = filename.relative_to(root)
+
             entries[timestamp] = {
                 "timestamp": timestamp.strftime("%Y-%m-%d %H:%M:%S"),
                 "path": filename,
