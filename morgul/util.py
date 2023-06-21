122c9b646f8a3dcfff198c1050c034c6e05eb6e0
Nicholas Devenish <ndevenish@gmail.com>
2023-06-21 15:09:38 +0100

    Add calibration-log-finder to morgul.util

--- morgul/util.py
+++ morgul/util.py
@@ -1,5 +1,13 @@
+from __future__ import annotations
+
+import os
 import re
 import time
+from datetime import datetime, timedelta
+from pathlib import Path
+from typing import Literal
+
+import dateutil.tz as tz
 
 BOLD = "\033[1m"
 R = "\033[31m"
@@ -20,3 +28,88 @@ def elapsed_time_string(start_time: float) -> str:
 
 def strip_escapes(input: str) -> str:
     return re.sub("\033" + r"\[[\d;]+m", "", input)
+
+
+def read_calibration_file(
+    filter: Literal["PEDESTAL"] | Literal["MASK"],
+) -> dict[datetime, tuple[float, Path]]:
+    """Read the calibration log (if it exists) and get all entries"""
+    if "JUNGFRAU_CALIBRATION_LOG" not in os.environ:
+        raise RuntimeError(
+            "Could not find calibration log; please set JUNGFRAU_CALIBRATION_LOG"
+        )
+
+    entries: dict[datetime, tuple[float, Path]] = {}
+    for line in Path(os.environ["JUNGFRAU_CALIBRATION_LOG"]).read_text().splitlines():
+        if not line.startswith(filter):
+            continue
+        _, ts, exposure, filename = line.split()
+        timestamp = datetime.fromisoformat(ts)
+        entries[timestamp] = (float(exposure), Path(filename))
+    return entries
+
+
+def _convert_ts_to_utc_datetime(ts: float | datetime) -> datetime:
+    # Make sure we have a UTC timestamp
+    if isinstance(ts, datetime):
+        if ts.tzinfo is None:
+            return ts.replace(tzinfo=tz.UTC)
+        else:
+            return ts
+    else:
+        return datetime.fromtimestamp(ts).replace(tzinfo=tz.UTC)
+
+
+def _find_entry(
+    kind: Literal["PEDESTAL"] | Literal["MASK"],
+    timestamp: float | datetime,
+    exposure: float | None = None,
+    *,
+    within_minutes: int | None = None,
+) -> Path:
+    timestamp = _convert_ts_to_utc_datetime(timestamp)
+    lookup = read_calibration_file(kind)
+    candidates = [x for x in sorted(lookup) if x < timestamp]
+    if not candidates:
+        raise RuntimeError(
+            f"Could not find {kind.title()} entry taken before {timestamp}"
+        )
+
+    if within_minutes is not None:
+        candidates = [
+            x for x in candidates if x > timestamp - timedelta(minutes=within_minutes)
+        ]
+        if not candidates:
+            raise RuntimeError(
+                f"Could not find {kind.title()} entry taken within {within_minutes} minutes before {timestamp}"
+            )
+
+    if exposure is not None:
+        candidates = [x for x in candidates if lookup[x][0] == exposure]
+        if not candidates:
+            raise RuntimeError(f"Could not find entry taken with exposure {exposure}")
+
+    # Return the latest filename
+    return lookup[candidates[-1]][1]
+
+
+def find_mask(
+    timestamp_utc: float | datetime,
+    exposure_time: float | None,
+    *,
+    within_minutes: int | None = None,
+) -> Path | None:
+    return _find_entry(
+        "MASK", timestamp_utc, exposure_time, within_minutes=within_minutes
+    )
+
+
+def find_pedestal(
+    timestamp_utc: float | datetime,
+    exposure_time: float | None,
+    *,
+    within_minutes: int | None = None,
+) -> Path:
+    return _find_entry(
+        "PEDESTAL", timestamp_utc, exposure_time, within_minutes=within_minutes
+    )
