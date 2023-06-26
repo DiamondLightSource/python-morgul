bc7fbc5c7558dad425787e9f90c008761e42620a
Nicholas Devenish <ndevenish@gmail.com>
2023-06-26 12:29:42 +0100

    Watcher: Start putting in infrastructure for bad files

--- morgul/watcher/morgul_watch.py
+++ morgul/watcher/morgul_watch.py
@@ -1,6 +1,7 @@
 import datetime
 import logging
 import os
+import re
 import subprocess
 import sys
 import time
@@ -12,12 +13,15 @@ import dateutil.tz as tz
 import h5py
 import typer
 
+from morgul.util import NC, B, G, R
 from morgul.watcher.watcher import Watcher
 
 logger = logging.getLogger(__name__)
 
 # Only consider files matching
 FILTER_REGEX = r".+\.h5"
+# FILTER_REGEX = r".+2023-06-26.+\/.+\.h5|/+ssx"
+
 # A cache file so that we can quickly re-present results
 CACHE_FILE = ".watcher_history"
 # Time to sleep between scans
@@ -60,6 +64,7 @@ def read_h5_info(filename: Path) -> dict[str, Any] | None:
             "exptime": exptime,
             "gainmode": f["gainmode"][()].decode(),
             "nimage": f"{f['data'].shape[0]:5}",
+            "bad": False,
         }
 
 
@@ -71,15 +76,75 @@ class EmitHandler:
 
     def __init__(self, output_stream: IO = sys.stdout, *, fzf: bool = False):
         self._entries = {}
+        self._entry_order = []
         self._fzf = fzf
         self._output_stream = output_stream
         self._longest_path = 0
+        self._lines_to_update = set()
 
     def set_output_stream(self, stream: IO) -> None:
         self._output_stream = stream
 
     def print(self, *args, sep: str = " ", end: str = "\n") -> None:
         self._output_stream.write(sep.join(str(x) for x in args) + end)
+
+    def _generate_entry_line(self, entry: dict[str, Any]) -> str:
+        path_length = min(self._longest_path, 87)
+        filename = entry["filename"].resolve()
+        # Handle unicode prettiness
+        prefix = "┃"
+        # Handle dividers if our new entry is in a different folder
+        if filename.parent != self._last_folder:
+            if self._last_folder is not None:
+                # Tie it off
+                self.print(
+                    ("- " if self._fzf else "  ")
+                    + "┗"
+                    + "━" * 21
+                    + "┷"
+                    + "━" * (path_length + 27)
+                    + "┛"
+                )
+            prefix = "┏"
+            self._last_folder = filename.parent
+
+        dec = [" " if not self._fzf else str(entry["filename"].resolve()), prefix]
+        # if filename in self._entry_order:
+        # Manage the colour
+        if entry["bad"]:
+            # We want to only show partial information
+            return (
+                " ".join(
+                    [
+                        *dec,
+                        R
+                        + entry["timestamp"].astimezone().strftime("%Y-%m-%d %H:%M:%S")
+                        + NC,
+                        "│",
+                        R
+                        + str(
+                            entry["filename"].relative_to(Settings.get().root_path)
+                        ).ljust(path_length),
+                        # entry["reason"] + NC,
+                    ]
+                )
+                + NC
+            )
+
+        return " ".join(
+            str(x)
+            for x in [
+                *dec,
+                entry["timestamp"].astimezone().strftime("%Y-%m-%d %H:%M:%S"),
+                "│",
+                str(entry["filename"].relative_to(Settings.get().root_path)).ljust(
+                    path_length
+                ),
+                entry["gainmode"].ljust(13),
+                f"{entry['exptime']*1000:4g}ms",
+                str(entry["nimage"]).ljust(5),
+            ]
+        )
 
     def emit_new_entries(self, entries: list[dict[str, Any]]) -> None:
         # Check to see if we need to extend the longest path for these
@@ -91,40 +156,9 @@ class EmitHandler:
             ],
         )
 
-        path_length = min(self._longest_path, 87)
-
         for entry in sorted(entries, key=lambda x: x["timestamp"]):
             # For now, just emit the lines
-            filename = entry["filename"]
-            # Handle unicode prettiness
-            prefix = "┃"
-            # Handle dividers if our new entry is in a different folder
-            if filename.parent != self._last_folder:
-                if self._last_folder is not None:
-                    # Tie it off
-                    self.print(
-                        ("- " if self._fzf else "  ")
-                        + "┗"
-                        + "━" * 21
-                        + "┷"
-                        + "━" * (path_length + 27)
-                        + "┛"
-                    )
-                prefix = "┏"
-                self._last_folder = filename.parent
-
-            dec = [" " if not self._fzf else str(entry["filename"].resolve()), prefix]
-            self.print(
-                *dec,
-                entry["timestamp"].astimezone().strftime("%Y-%m-%d %H:%M:%S"),
-                "│",
-                str(entry["filename"].relative_to(Settings.get().root_path)).ljust(
-                    path_length
-                ),
-                entry["gainmode"].ljust(13),
-                f"{entry['exptime']*1000:4g}ms",
-                str(entry["nimage"]).ljust(5),
-            )
+            self.print(self._generate_entry_line(entry))
 
     def reemit(self) -> None:
         """Print everything again"""
@@ -150,6 +184,10 @@ def watch(
         ),
     ] = None,
     use_fzf: Annotated[bool, typer.Option("--fzf")] = False,
+    wait: Annotated[
+        bool,
+        typer.Option("--wait", help="If the root path does not exist, wait for it to."),
+    ] = False,
 ):
     """Watch a data folder for new files appearing"""
 
@@ -181,14 +219,25 @@ def watch(
     # Update the singleton root object
     Settings.get().root_path = root_path
 
+    if not root_path.is_dir():
+        if not wait:
+            print(f"{R}Error: Root path {root_path} does not exist{NC}")
+            raise typer.Abort()
+        else:
+            print(f"Root path {B}{root_path}{NC} does not exist, waiting,")
+            while not root_path.exists():
+                time.sleep(5)
+
     logger.debug(f"Starting watch on {root_path}")
     watcher = Watcher(root_path)
 
     # Keep track of files we couldn't open yet, with a timestamp so we don't get stuck
     unscanned_files: set[Path] = set()
 
+    first_scan = True
     if not use_fzf:
         print("Doing initial scan, this could take a minute...")
+        start_time = time.monotonic()
 
     if use_fzf:
         fzf = subprocess.Popen(
@@ -210,14 +259,30 @@ def watch(
     else:
         handler = EmitHandler(sys.stdout)
 
+    re_filter = re.compile(FILTER_REGEX)
     while True:
         new_files, dropped_paths = watcher.scan()
         scan_time = time.monotonic()
+
+        if first_scan:
+            print(f"    Found {G}{len(new_files)}{NC} files, reading...")
+
+        # Drop any unscanned files in any dropped paths
+        for filename in list(unscanned_files):
+            if any(x in filename.parents for x in dropped_paths):
+                logger.debug(
+                    f"Dropping unscanned file {filename} because path is dropped"
+                )
+                unscanned_files.remove(filename)
 
         # Store a list of processed - we may want to group/reorder once we have metadata
         processed = []
         # For each new file, open it and get some details
         for filename in sorted(new_files + list(unscanned_files)):
+            # Filter the filenames
+            if not re_filter.match(str(filename)):
+                logger.debug(f"Ignoring file {filename} as does not match filter")
+                continue
             try:
                 data = read_h5_info(filename)
                 if not data:
@@ -227,9 +292,28 @@ def watch(
                 if filename in unscanned_files:
                     unscanned_files.remove(filename)
 
-            except (IOError, KeyError):
+            except (IOError, KeyError) as e:
                 unscanned_files.add(filename)
+                # processed.append(
+                #     {
+                #         "filename": filename,
+                #         "bad": True,
+                #         # Some fake "In the future" timestamp
+                #         "timestamp": datetime.datetime.fromtimestamp(
+                #             filename.stat().st_mtime
+                #         ).astimezone(),
+                #         "reason": str(e),
+                #     }
+                # )
                 continue
+
+        if first_scan:
+            first_scan = False
+            print(f"    kept {G}{len(processed)}{NC} files.")
+            print(f"    done in {G}{1000*(time.monotonic()-start_time):g}{NC} ms")
+            if not new_files:
+                print("No files found on first scan.")
+            print()
 
         if processed:
             handler.emit_new_entries(processed)
